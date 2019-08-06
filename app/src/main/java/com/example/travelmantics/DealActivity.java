69266ad7435ce247721c4ddba1d796e22a0f0394
George Tsiga <tsiga.george@gmail.com>
2019-08-06 22:58:04 +0200

    Updated Gradle plugins version Removed duplicate method Did some formatting

--- app/src/main/java/com/example/travelmantics/DealActivity.java
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -28,28 +28,27 @@ import com.squareup.picasso.Picasso;
 public class DealActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
-    private static final int PICTURE_RESULT=42;
+    private static final int PICTURE_RESULT = 42;
     EditText txtTitle;
     EditText txtDescription;
     EditText txtPrice;
     TravelDeal deal;
     ImageView ImageView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deal);
-//        FirebaseUtil.openFbReference("traveldeals",this);
+        //        FirebaseUtil.openFbReference("traveldeals",this);
         mFirebaseDatabase = FirebaseUtil.mFirebasedatabase;
         mDatabaseReference = FirebaseUtil.mDatabaseReference;
         txtTitle = (EditText) findViewById(R.id.txtTitle);
         txtDescription = (EditText) findViewById(R.id.txtDescription);
         txtPrice = (EditText) findViewById(R.id.txtPrice);
-        ImageView = (ImageView)findViewById(R.id.image);
+        ImageView = (ImageView) findViewById(R.id.image);
         final Intent intent = getIntent();
         TravelDeal deal = (TravelDeal) intent.getSerializableExtra("Deal");
-        if (deal==null){
+        if (deal == null) {
             deal = new TravelDeal();
         }
         this.deal = deal;
@@ -64,24 +63,22 @@ public class DealActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/jpeg");
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(intent.createChooser(intent,
-                        "Insert Picture"),PICTURE_RESULT);
+                startActivityForResult(intent.createChooser(intent, "Insert Picture"), PICTURE_RESULT);
             }
         });
-
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.save_menu:
-            saveDeal();
-            Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
-            clean();
-            return true;
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
             case R.id.delete_menu:
                 deletedeal();
-                Toast.makeText(this,"Deal Deleted",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Deal Deleted", Toast.LENGTH_LONG).show();
                 backTolist();
                 return true;
             default:
@@ -89,20 +86,16 @@ public class DealActivity extends AppCompatActivity {
         }
     }
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
 
-        if (FirebaseUtil.isAdmin){
+        if (FirebaseUtil.isAdmin) {
             menu.findItem(R.id.delete_menu).setVisible(true);
             menu.findItem(R.id.save_menu).setVisible(true);
             enableEditText(true);
-        }
-
-        else {
+        } else {
             menu.findItem(R.id.delete_menu).setVisible(false);
             menu.findItem(R.id.save_menu).setVisible(false);
             enableEditText(false);
@@ -113,7 +106,7 @@ public class DealActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==PICTURE_RESULT && resultCode==RESULT_OK){
+        if (requestCode == PICTURE_RESULT && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
             final StorageReference ref = FirebaseUtil.mStorageRef.child(imageUri.getLastPathSegment());
             ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -123,7 +116,7 @@ public class DealActivity extends AppCompatActivity {
                         @Override
                         public void onSuccess(Uri uri) {
                             String Url = uri.toString();
-                            Log.d("Travel","Download Url = "+ Url);
+                            Log.d("Travel", "Download Url = " + Url);
                             deal.setImageUrl(Url);
                         }
                     });
@@ -136,50 +129,41 @@ public class DealActivity extends AppCompatActivity {
         deal.setTitle(txtTitle.getText().toString());
         deal.setTitle(txtDescription.getText().toString());
         deal.setTitle(txtPrice.getText().toString());
-        if (deal.getId()==null)
-        mDatabaseReference.push().setValue(deal);
-        else {
+        if (deal.getId() == null) { mDatabaseReference.push().setValue(deal); } else {
             mDatabaseReference.child(deal.getId()).setValue(deal);
         }
     }
 
-    private void deletedeal(){
-        if (deal ==null){
-            Toast.makeText(this,"please save the deal before deleting", Toast.LENGTH_SHORT).show();
+    private void deletedeal() {
+        if (deal == null) {
+            Toast.makeText(this, "please save the deal before deleting", Toast.LENGTH_SHORT).show();
             return;
         }
         mDatabaseReference.child(deal.getId()).removeValue();
-
     }
 
-    private void backTolist(){
-        Intent intent =new Intent(this, ListActivity.class);
+    private void backTolist() {
+        Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
     }
-    private void clean(){
+
+    private void clean() {
         txtTitle.setText("");
         txtDescription.setText("");
         txtPrice.setText("");
         txtTitle.requestFocus();
-
     }
-    private void enableEditText(boolean isEnabled){
+
+    private void enableEditText(boolean isEnabled) {
         txtTitle.setEnabled(isEnabled);
         txtDescription.setEnabled(isEnabled);
         txtPrice.setEnabled(isEnabled);
     }
-    private void showImage(String url){
-        if (url !=null && url.isEmpty() == false){
-            int with = Resources.getSystem().getDisplayMetrics().widthPixels;
-            Picasso.with(this)
-                    .load(url)
-                    .resize(width, width*2/3)
-                    .centerCrop()
-                    .into(ImageView);
 
+    private void showImage(String url) {
+        if (url != null && url.isEmpty() == false) {
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.with(this).load(url).resize(width, width * 2 / 3).centerCrop().into(ImageView);
         }
     }
-
-
-
 }
